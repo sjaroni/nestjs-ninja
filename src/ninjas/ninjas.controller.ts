f7b19a52d1ba6a066872256e6460ccd58380ec46
Stefan Jaroni <sjaroni@outlook.de>
2025-03-17 23:19:57 +0100

    feat: add routes in controller, add dtos

--- src/ninjas/ninjas.controller.ts
+++ src/ninjas/ninjas.controller.ts
@@ -1,4 +1,50 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, Query } from '@nestjs/common';
+import { CreateNinjaDto } from './dto/create-ninja.dto';
+import { UpdateNinjaDto } from './dto/update-ninja.dto';
 
 @Controller('ninjas')
-export class NinjasController {}
+export class NinjasController {
+  
+  // GET /ninjas?type=fast --> []
+  @Get()
+  getNinjas(@Query('type') type: string) {
+    return [
+      {
+        type
+      }
+    ];
+  }
+
+  // GET /ninjas/:id --> { ... }
+  @Get(':id')
+  getSingleNinja(@Param('id') id: string) {
+    return {
+      id
+    };
+  }
+
+  // POST /ninjas
+  @Post()
+  createNinja(@Body() createNinjaDto: CreateNinjaDto) {
+    return {
+      name: createNinjaDto.name
+    };
+  }
+
+  // PUT /ninjas/:id --> { ... }
+  @Put(':id')
+  updateNinja(@Param('id') id: string, @Body() updateNinjaDto: UpdateNinjaDto) {
+    return {
+      id,
+      name: updateNinjaDto
+    };
+  }
+
+  // DELETE /ninjas/:id
+  @Delete(':id')
+  deleteNinja(@Param('id') id: string) {
+    return {
+      id
+    };
+  }
+}
